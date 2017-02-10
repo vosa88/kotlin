d5bc1ae0c6f75abd93ed2e479658b83fe8443877
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-02-10 20:35:09 +0300

    Add extension CompilerConfiguration.languageVersionSettings for convenience

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/project/Platform.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/project/Platform.kt
@@ -136,7 +136,7 @@ private fun getExtraLanguageFeatures(
 }
 
 fun KtElement.createCompilerConfiguration(): CompilerConfiguration = CompilerConfiguration().apply {
-    put(CommonConfigurationKeys.LANGUAGE_VERSION_SETTINGS, languageVersionSettings)
+    languageVersionSettings = this@createCompilerConfiguration.languageVersionSettings
     isReadOnly = true
 }
 
